ba89e09995fa38cdc4b1bf02dc4907d887701198
Rayat-7 <raisoulrayat@gmail.com>
2025-03-28 23:59:28 +0600

    update layout and add search functionality; replace fonts and remove unused assets

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 
 export default function layout({children}:Readonly<{children:React.ReactNode}>){
     return (
-        <main className="font-work-sans">
+        <main className="font-work-sans w-full">
             <Navbar />
             {children}
         </main>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import SearchForm from "../components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}:{
+  searchParams:Promise<{query?:string}>
+}) {
+  const query =(await searchParams).query;
   return (
     <>
-    <h1 className="2xl">Home</h1>
+    
+    <section className="pink_container">
+      <h1 className="heading">Pitch you startup <br/> Connect with Entreprenuers </h1>
+      <p className="sub-heading">Submit Ideas ,vote on pitches and Get Noticed in Virtual Competitions</p>
+      <SearchForm query={query}/>
+    
+    </section>
+
     </>
   );
 }
